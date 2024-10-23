334ab16b69a4015725cdde9d4f5c679d084a9ba0
Michelle Xing <xingx139@newschool.edu>
2024-10-23 17:46:42 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [count, setCount] = useState(1);
+  const [posts, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
 
 
-
-function Form() {
-  const [name, setName] = useState('');
-  const [age, setAge] = useState('');
-  const [pronoun, setPronoun] = useState('');
-  const [entries, setEntries] = useState([]);
-
-  const handleAdd = () => {
-    if (name.trim() && age.trim()) {
-      setEntries([...entries, { name, age, pronoun}]);
-      setName('');
-      setAge('');
-      setPronoun('');
-    }
-  };
+  useEffect(() => {
+    // fetching the post data based on the count value
+    fetch(`https://data.ny.gov/resource/w8wg-vp5f.json/`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setPost(data)
+        setLoading(false)
+      })
+      .catch(error => console.error('Error fetching post:', error));
+  }, []); // The effect depends on the count
 
   return (
     <div>
-      <form>
-        <label>
-          Name:
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <label>
-          Age:
-          <input
-            type="number"
-            value={age}
-            onChange={(e) => setAge(e.target.value)}
-          />
-        </label>
-        <label>
-          Pronoun:
-          <input
-            type="text"
-            value={pronoun}
-            onChange={(e) => setPronoun(e.target.value)}
-          />
-        </label>
-        <button type="button" onClick={handleAdd}>
-          Add
-        </button>
-      </form>
+      <p>Address Type: {count}</p>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>
+        Next Address
+      </button>
+      {loading ? <p>Loading...</p> : (
+        <ul>
+          {posts.map((post, i) => (
+            <div key={i}>
+              <h2>{post.borough}</h2>
+              <p>{post.intersection_street_1}</p>
+              <p>{post.intersection_street_2}</p>
+              <p>{post.latitude}{post.longitude}</p>
+            </div>
 
-      <ul>
-        {entries.map((entry, index) => (
-          <li key={index}>
-            {entry.name} is {entry.age} years old. {entry.pronoun}
-          </li>
-        ))}
-      </ul>
+          ))}
+        </ul>
+      )}
+
     </div>
   );
 }
 
-export default Form;
+export default App
